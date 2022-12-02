9be4ab3a744637c8aa1e73eec483dde368b98597
1327-Imai <k021g1327@g.neec.ac.jp>
2022-12-02 15:22:17 +0900

    string型でいったん作成

--- main.cpp
+++ main.cpp
@@ -1,126 +1,72 @@
 #include <stdio.h>
-#include <windows.h>
-#include <random>
-#include <functional>
-
-typedef void (*PVoidFunc)(int);
-typedef int (*PIntFunc)(int);
-
-//関数のプロトタイプ宣言
-//偶数か奇数かを問う関数
-void Question(int& inputNum);
-
-//秒単位をミリ秒単位として扱うようにする関数
-int ConvertMilliSec(int time);
-
-//乱数を生成する関数
-int CreateRandomNum(float min , float max);
-
-//メッセージを表示する関数
-void DisplayTimerMessage(int s);
-
-//指定した秒数待つ関数
-void SetTimeOut(PVoidFunc pVoid ,PIntFunc pInt, int s);
-
-//正解か調べる関数
-void CheckCorrectAnswer(int inputNum , int randomNum);
+#include <vector>
+#include <list>
+#include <iostream>
 
 //main関数
 int main() {
 
-	//変数の宣言と初期化
-	int inputNum = 0;
-	int randomNum = 0;
-	const int time = 3;
+	int year = 1970;
 
-	PVoidFunc pVoid = DisplayTimerMessage;
-	PIntFunc pInt = ConvertMilliSec;
+	std::list<std::string>yamanoteLine{
+		{"Shinagawa"} ,
+		{"Osaki"} ,
+		{"Gotanda"} ,
+		{"Meguro"} ,
+		{"Ebisu"} ,
+		{"Shibuya"} ,
+		{"Harajuku"} ,
+		{"Yoyogi"} ,
+		{"Shinjuku"} ,
+		{"Shin-Okubo"} ,
+		{"Takadanobaba"} ,
+		{"Ikebukuro"} ,
+		{"Otuka"} ,
+		{"Sugamo"} ,
+		{"Komagome"} ,
+		{"Tabata"} ,
+		{"Nipori"} ,
+		{"Uguisudani"} ,
+		{"Ueno"} ,
+		{"Okachimachi"} ,
+		{"Akihabara"} ,
+		{"Kanda"} ,
+		{"Tokyo"} ,
+		{"Yurakucho"} ,
+		{"Shimbashi"} ,
+		{"Hamamatsucho"} ,
+	};
 
-	Question(inputNum);
+	printf("year:%d\n" , year);
+	for (std::list<std::string>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); itr++) {
+		std::cout << *itr << '\n';
+	}
+	putchar('\n');
 
-	randomNum = CreateRandomNum(1,100);
 
-	SetTimeOut(pVoid , pInt , time);
+	year = 2019;
+	for (std::list<std::string>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); itr++) {
+		if (*itr == "Nipori") {
+			itr = yamanoteLine.insert(itr , "Nishi-Nippori");
+			++itr;
+		}
+	}
 
-	CheckCorrectAnswer(inputNum , randomNum);
+	printf("year:%d\n" , year);
+	for (std::list<std::string>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); itr++) {
+		std::cout << *itr << '\n';
+	}
+	putchar('\n');
+
+	year = 2022;
+	yamanoteLine.push_back("TakanawaGatway");
+
+	printf("year:%d\n" , year);
+	for (std::list<std::string>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); itr++) {
+		std::cout << *itr << '\n';
+	}
+	putchar('\n');
 
 	return 0;
-
-}
-
-//関数の定義
-//偶数か奇数かを問う関数
-void Question(int& inputNum) {
-
-	while (true) {
-
-		printf("奇数か偶数かを選んでください。\n");
-		printf("奇数:1\n");
-		printf("偶数:2\n");
-
-		scanf_s("%d" , &inputNum);
-
-		if (inputNum == 1 || inputNum == 2) {
-			break;
-		}
-		else {
-			printf("エラー:無効な入力です\n");
-		}
-
-	}
-}
-
-//秒単位をミリ秒単位として扱うようにする関数
-int ConvertMilliSec(int time) {
-	return time * 1000;
-}
-
-//乱数を生成する関数
-int CreateRandomNum(float min , float max) {
-	//乱数シード生成器
-	std::random_device seed_gen;
-	//メルセンヌ・ツイスターの乱数エンジン
-	std::mt19937_64 engine(seed_gen());
-	//乱数範囲の設定
-	std::uniform_real_distribution<float> dist(min , max);
-
-	return dist(engine);
-}
-
-//メッセージを表示する関数
-void DisplayTimerMessage(int s) {
-
-	printf("判定中です。(待ち時間:%d秒)\n" , s);
-
-}
-
-//指定した秒数待つ関数
-void SetTimeOut(PVoidFunc pVoid, PIntFunc pInt ,int s) {
-
-	pVoid(s);
-
-	Sleep(pInt(s));
-}
-
-//正解か調べる関数
-void CheckCorrectAnswer(int inputNum , int randomNum) {
-
-	int answer = 0;
-
-	if (randomNum % 2 == 1) {
-		answer = 1;
-	}
-	else {
-		answer = 2;
-	}
-
-	if (inputNum == answer) {
-		printf("正解です、おめでとうございます。\n");
-		printf("生成された数字:%d\n" , randomNum);
-	}
-	else {
-		printf("残念、不正解でした。\n");
-		printf("生成された数字:%d\n" , randomNum);
-	}
 
 }
