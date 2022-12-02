5b407309d345dbb1d7f0579e3a402a8240ae5cda
1327-Imai <k021g1327@g.neec.ac.jp>
2022-12-02 16:05:32 +0900

    資料02-03までの処理を追加

--- main.cpp
+++ main.cpp
@@ -1,34 +1,43 @@
 #include <stdio.h>
-#include <vector>
-#include <list>
-#include <iostream>
 #include <stdlib.h>
+#include <string.h>
 
 //単方向リスト構造体
 typedef struct Cell {
 
 	char str[16];
+	struct Cell* prev;
 	struct Cell* next;
 
 };
 
 
-void create(Cell* head , const char* str);
-void index(Cell* head);
+void create(Cell* currentCell , const char* str);
+void index(Cell* endCell);
+Cell* GetInsertCellAddress(Cell* endCell , int iterator);
 
 //main関数
 int main() {
 
+	int iterator;
 	char str[16];
+	Cell* insertCell;
 
 	//先頭のセルを宣言
 	Cell head;
+	head.prev = nullptr;
 	head.next = nullptr;
 
 	while (true) {
 
+		printf("何番目のセルの後に挿入しますか?\n");
+		scanf_s("%d" , &iterator);
+
+		printf("挿入する文字列を入力してください\n");
 		scanf_s("%s" , str , 16);
 
+		printf("リストを表示します\n");
+		insertCell = GetInsertCellAddress(&head,iterator);
 		create(&head , str);
 
 		index(&head);
@@ -39,7 +48,7 @@ int main() {
 
 }
 
-void create(Cell* head , const char* str) {
+void create(Cell* currentCell , const char* str) {
 
 	Cell* newCell;
 
@@ -48,23 +57,39 @@ void create(Cell* head , const char* str) {
 
 	//引数を新規作成したセルの値に入れる
 	strcpy_s(newCell->str ,16 , str);
-	newCell->next = nullptr;
+	
+	newCell->prev = currentCell;
+	newCell->next = currentCell->next;
 
-	while (head->next != nullptr) {
-		head = head->next;
+	if (currentCell->next) {
+		Cell* nextCell = currentCell->next;
+		nextCell = newCell;
 	}
 
-	head->next = newCell;
+	currentCell->next = newCell;
 
 }
 
-void index(Cell* head) {
+void index(Cell* endCell) {
 
-	while (head->next != nullptr) {
-		head = head->next;
-		printf("%s\n" , head->str);
+	while (endCell->next != nullptr) {
+		endCell = endCell->next;
+		printf("%s\n" , endCell->str);
 	}
 
 	putchar('\n');
 
+}
+
+Cell* GetInsertCellAddress(Cell* endCell , int iterator) {
+
+	for (int i = 0; i < iterator; i++) {
+		if (endCell->next) {
+			endCell = endCell->next;
+		}
+		else {
+			break;
+		}
+	}
+	return endCell;
 }
